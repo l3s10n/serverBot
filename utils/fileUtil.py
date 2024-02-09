821625f1450fd1dac2565624407a2baf3688cdfb
Your Name <you@example.com>
2024-02-09 19:43:58 +0800

    fix bug of auto create logs

--- utils/fileUtil.py
+++ utils/fileUtil.py
@@ -3,6 +3,6 @@ import os
 def fileAppend(filePath, content):
     dirPath = os.path.dirname(filePath)
     if not os.path.exists(dirPath):
-        os.mkdirs(dirPath)
+        os.makedirs(dirPath)
     with open(filePath, 'a') as f:
         f.write(content)
