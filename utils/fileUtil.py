76667c8b959292be6063d44d3847f361237fa3fc
Your Name <you@example.com>
2024-02-09 19:42:04 +0800

    add auto create logs

--- utils/fileUtil.py
+++ utils/fileUtil.py
@@ -1,3 +1,8 @@
+import os
+
 def fileAppend(filePath, content):
+    dirPath = os.path.dirname(filePath)
+    if not os.path.exists(dirPath):
+        os.mkdirs(dirPath)
     with open(filePath, 'a') as f:
         f.write(content)
